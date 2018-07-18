ea09c6737a612041bec8d2ab92c683735b02643d
Christopher Pitts <cphpitts@gmail.com>
2018-07-18 11:47:40 -0700

    Version 1.0 Live

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,21 @@
 const gameContainer = document.getElementById("gameNames");
 var userNameList = [];
+var xmltest
+
 
 function loadLists() {
-	console.log("Running Load Lists");
-	// console.log(userNameList[0].value)
-	userNameList = document.querySelectorAll("#userNamesContainer input");
+	console.log("Running Load Lists");	userNameList = document.querySelectorAll("#userNamesContainer input");
 	if (userNameList[0].value == '') {
 		var userList = userQuery();
 	} else {
 		var userList = userInput();
 	}
-	// console.log('userList: ' + userList)
 	if (userList == undefined) {
 		noUsers();
 	} else {
-		loadCollection(userList);
+		var gameCollection = loadCollection(userList);
+		//gameCollection = loadAdditionalInfo(gameCollection);
+		populateGameList(gameCollection);
 	}
 }
 
@@ -32,9 +33,7 @@ function userInput() {
 	for (i=0; i<userListLength; i++) {
 		userList[i] = userNameList[i].value;
 	}
-	// console.log(userList)
 	return userList;
-
 }
 
 function addUser() {
@@ -45,7 +44,7 @@ function addUser() {
 
 function loadCollection(users) {
 	userNumber = users.length;
-	var finalCollectionID = new Array();
+	var finalCollection = new Array();
 
 	req = new Array();
 
@@ -66,38 +65,101 @@ function loadCollection(users) {
 		userGamesData[i] = parser.parseFromString(userRaw[i], "text/xml");
 		var userGameLength = userGamesData[i].getElementsByTagName("item").length;
 		var userGamesInfo = userGamesData[i].getElementsByTagName("item");
-		var currentListLength = finalCollectionID.length;
+		var currentListLength = finalCollection.length;
 		for (j=0; j<userGameLength; j++) {
-			finalCollectionID.push(new Object());
-			finalCollectionID[currentListLength+j].gameID = userGamesInfo[j].attributes.getNamedItem('objectid').value;
-			finalCollectionID[currentListLength+j].gameName = userGamesInfo[j].getElementsByTagName('name')[0].innerHTML;
-			finalCollectionID[currentListLength+j].gameImage = userGamesInfo[j].getElementsByTagName('thumbnail')[0].innerHTML;
-			finalCollectionID[currentListLength+j].gameOwner = users[i];
+			// console.log(j);
+			finalCollection.push(new Object());
+			finalCollection[currentListLength+j].gameID = userGamesInfo[j].attributes.getNamedItem('objectid').value;
+			finalCollection[currentListLength+j].gameName = userGamesInfo[j].getElementsByTagName('name')[0].innerHTML;
+			finalCollection[currentListLength+j].gameImage = userGamesInfo[j].getElementsByTagName('thumbnail')[0].innerHTML;
+			finalCollection[currentListLength+j].gameOwner = users[i];
 		}
+	}
 
-		// Sort List
-		finalCollectionID.sort(function(a,b){
-			var keyA = Number(a.gameName), keyB = Number(b.gameName);
-			if (keyA< keyB) return -1;
-			if (keyB< keyA) return 1;
-			return 0
-		});
+	// Sort List
+	finalCollection.sort(function(a,b){
+		var keyA = a.gameName, keyB = b.gameName;
+		if (keyA< keyB) return -1;
+		if (keyB< keyA) return 1;
+		return -1
+	});
 
-		//remove Duplicates
-		currentListLength = finalCollectionID.length;
-		var k = 0;
-		console.log(finalCollectionID[k].gameID);
-		console.log(finalCollectionID[k+1].gameID);
-		while (k+1<currentListLength) {
-			if (finalCollectionID[k].gameID === finalCollectionID[k+1].gameID) {
-				finalCollectionID[k].gameOwner = finalCollectionID[k].gameOwner + ', ' + finalCollectionID[k+1].gameOwner;
-				currentListLength --;
-				delete finalCollectionID[k+1];
-			} else {
-				k++;
-			}
+	//remove Duplicates
+	currentListLength = finalCollection.length;
+	var k = 0;
+	while (k<currentListLength-1) {
+		if (finalCollection[k].gameID === finalCollection[k+1].gameID) {
+			finalCollection[k].gameOwner = finalCollection[k].gameOwner + ', ' + finalCollection[k+1].gameOwner;
+			currentListLength--;
+			finalCollection.splice(k+1,1);
+			// delete finalCollection[k+1];
+		} else {
+			k++;
 		}
+	}
+		console.log(finalCollection);
+	return finalCollection;
+
+}
+
+// Load Additional Game Information
+function loadAdditionalInfo(gameList) {
+	gameNumber = gameList.length;
+	var req, gameRaw, gameData;
+	for (i=0; i<gameNumber; i++) {
+		var gameXML = "https://www.boardgamegeek.com/xmlapi2/thing?id=" + gameList[i].gameID;
+		req = new XMLHttpRequest();
+		req.open("GET", gameXML, false);
+		dataType: "jsonp"
+		req.send(null);
+
+		var parser, xmlDoc;
+
+		parser = new DOMParser();
+
+		gameRaw = req.responseText;
+		gameData = parser.parseFromString(gameRaw, "text/xml");
+
+		console.log(gameData);
+		console.log(gameData.childNodes[0].childNodes[0].getElementsByTagName('minplayers')[0].attributes.value.value);
+		xmltest = gameData;
+		gameList[i].minPlayer = gameData.childNodes[0].childNodes[0].getElementsByTagName('minplayers')[0].attributes.value.value;
+		gameList[i].maxPlayer = gameData.childNodes[0].childNodes[0].getElementsByTagName('maxplayers')[0].attributes.value.value;
+	}
+	console.log(gameList);
+}
+
+//Populate Page
+function populateGameList(gameList) {
+	currentLength = gameList.length;
+	var mainDiv = document.getElementById("gameNames");
+	mainDiv.innerHTML = "";
+	for (i=0; i<currentLength; i++) {
+		var gameContainer = document.createElement("div");
+		gameContainer.className = "gameContainer";
+		mainDiv.appendChild(gameContainer);
+		if (i % 2 == 0) {
+			gameContainer.className += " gameEven";
+		}
+		var gameImage = document.createElement("div");
+		gameContainer.appendChild(gameImage);
+		gameImage.innerHTML = '<a href="https://www.boardgamegeek.com/boardgame/' + gameList[i].gameID + '">' + '<img src="' + gameList[i].gameImage + '"></a>';
+
+		var gameText = document.createElement("div");
+		gameText.className = "gameText";
+		gameContainer.appendChild(gameText);
+
+		// Game Name
+		var gameName = document.createElement("div");
+		gameName.className = "gameName";
+		gameText.appendChild(gameName);
+		gameName.innerHTML = '<a href="https://www.boardgamegeek.com/boardgame/' + gameList[i].gameID + '">' + gameList[i].gameName + '</a>';
+
+		// Game Owner
+		var gameOwner = document.createElement("div");
+		gameOwner.className = "gameOwner";
+		gameOwner.innerHTML = gameList[i].gameOwner;
+		gameText.appendChild(gameOwner);
 
 	}
-	console.log(finalCollectionID);
 }
